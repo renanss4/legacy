8d8b9183ab68e8d03e2ff8f1b3dafa32340b52f2
Renan Silva <renansantos.silva04@outlook.com>
2024-03-17 17:23:29 -0300

    Initial commit

---
+++ python/tdd/main.py
@@ -0,0 +1,8 @@
+# https://docs.python.org/3/library/unittest.html
+# https://docs.python.org/3/library/doctest.html
+
+from calculadora import soma
+
+print(soma(1.5, 2.5))
+print(soma(-10, 20))
+print(soma(100, 20))
