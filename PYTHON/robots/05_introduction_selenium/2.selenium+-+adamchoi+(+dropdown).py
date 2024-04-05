c513867a9743cc7171fba54a7515d1a9218456fb
Renan Silva <renansantos.silva04@outlook.com>
2024-04-05 15:49:34 -0300

    organizing

--- PYTHON/robots/05_introduction_selenium/2.selenium+-+adamchoi+(+dropdown).py
+++ PYTHON/robots/05_introduction_selenium/2.selenium+-+adamchoi+(+dropdown).py
@@ -1,29 +1,28 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import Select
 import pandas as pd
 import time
 
 # define the website to scrape and path where the chromediver is located
 website = 'https://www.adamchoi.co.uk/overs/detailed'
-path = '/Users/frankandrade/Downloads/chromedriver'  # write your path here
-service = Service(executable_path=path)  # selenium 4
-driver = webdriver.Chrome(service=service)  # define 'driver' variable
+path = '/Users/frank/Downloads/chromedriver' # write your path here
+# define 'driver' variable
+driver = webdriver.Chrome(path)
 # open Google Chrome with chromedriver
 driver.get(website)
 
 # locate and click on a button
-all_matches_button = driver.find_element(by='xpath', value='//label[@analytics-event="All matches"]')
+all_matches_button = driver.find_element_by_xpath('//label[@analytics-event="All matches"]')
 all_matches_button.click()
 
 # select dropdown and select element inside by visible text
-dropdown = Select(driver.find_element(by='id', value='country'))
+dropdown = Select(driver.find_element_by_id('country'))
 dropdown.select_by_visible_text('Spain')
 # implicit wait (useful in JavaScript driven websites when elements need seconds to load and avoid error "ElementNotVisibleException")
 time.sleep(3)
 
 # select elements in the table
-matches = driver.find_elements(by='xpath', value='//tr')
+matches = driver.find_elements_by_tag_name('tr')
 
 # storage data in lists
 date = []
@@ -33,13 +32,12 @@ away_team = []
 
 # looping through the matches list
 for match in matches:
-    date.append(match.find_element(by='xpath', value='./td[1]').text)
-    home = match.find_element(by='xpath', value='./td[2]').text
+    date.append(match.find_element_by_xpath('./td[1]').text)
+    home = match.find_element_by_xpath('./td[2]').text
     home_team.append(home)
     print(home)
-    score.append(match.find_element(by='xpath', value='./td[3]').text)
-    away_team.append(match.find_element(by='xpath', value='./td[4]').text)
-
+    score.append(match.find_element_by_xpath('./td[3]').text)
+    away_team.append(match.find_element_by_xpath('./td[4]').text)
 # quit drive we opened at the beginning
 driver.quit()
 
