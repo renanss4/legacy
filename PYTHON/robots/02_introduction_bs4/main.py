a51e2d383d711a89803a6a778757f035a881a6da
Renan Silva <renansantos.silva04@outlook.com>
2024-04-05 15:32:56 -0300

    removendo note

--- PYTHON/robots/02_introduction_bs4/main.py
+++ PYTHON/robots/02_introduction_bs4/main.py
@@ -1,34 +1,23 @@
-from bs4 import BeautifulSoup  # Importa a classe BeautifulSoup para fazer a análise do HTML
-import requests  # Importa a biblioteca requests para fazer requisições HTTP
+from bs4 import BeautifulSoup
+import requests
 
-# Define a URL do website que será acessado
+#################################################
+# Extracting a movie transcript
+#################################################
+
+# How To Get The HTML
 website = 'https://subslikescript.com/movie/Titanic-120338'
-
-# Faz uma requisição GET para obter o conteúdo HTML da página
-response = requests.get(website)
-
-# Extrai o conteúdo HTML da resposta da requisição
-content = response.text
-
-# Cria um objeto BeautifulSoup para analisar o conteúdo HTML
+result = requests.get(website)
+content = result.text
 soup = BeautifulSoup(content, 'lxml')
+# print(soup.prettify())  # prints the HTML of the website
 
-# Encontra o elemento <article> com a classe 'main-article'
+# Locate the box that contains title and transcript
 box = soup.find('article', class_='main-article')
-
-# Extrai o texto do primeiro elemento <h1> dentro do elemento 'box'
+# Locate title and transcript
 title = box.find('h1').get_text()
-
-# Imprime o título na tela
-# print(title)
-
-# Encontra o primeiro elemento <div> com a classe 'full-script' dentro do elemento 'box'
-# Extrai todo o texto desse elemento, removendo espaços em branco extras
-# Utiliza um espaço como separador entre as palavras
 transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
 
-# Imprime o texto do script na tela
-# print(transcript)
-
+# Exporting data in a text file with the "title" name
 with open(f'{title}.txt', 'w') as file:
     file.write(transcript)
