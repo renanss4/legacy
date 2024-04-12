6f11b82633808206e29cbb565446f5fa422c0171
Renan <renansantos.silva04@outlook.com>
2024-04-12 10:02:24 -0300

    let, const and var

--- js/udemy_course01/02_js_fundamentals_part1/script.js
+++ js/udemy_course01/02_js_fundamentals_part1/script.js
@@ -40,10 +40,41 @@ let children;
  * JavaScript has dynamic typing: We do not have to manyally define the data type of
  * the value stored in a variable. Instead, data types are determined automatically.
 
-*/
+
 console.log(typeof true);
 console.log(typeof 23);
 console.log(typeof 'Jonas');
 let year;
 console.log(typeof year);
 console.log(typeof null);
+*/
+
+// let, const and var
+
+let age = 30;
+age = 31;
+console.log(age);
+
+const birthYear = 1991;
+// birthYear = 1990; // It should not work. TypeError -> Assignment to constant variable
+
+// Dubt: does this create a let, const or var?
+// year = 1221
+// console.log(year = 2024)
+/**
+ * Response:
+ * JavaScript treats it as a global variable if it's in the global
+ * scope (outside of any function). If it's inside a function, it becomes a
+ * variable in the function's scope.
+ * 
+ * It's best practice to always declare your variables explicitly using
+ * let, const, or var to avoid potential issues with variable scoping and
+ * unintended global variables
+ */
+
+
+
+// var is basically the old way of defining variables, prior to ES6
+var job = 'programmer';
+job = 'teacher';
+console.log(job);
