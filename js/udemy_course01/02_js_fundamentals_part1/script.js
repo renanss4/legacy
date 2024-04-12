25b3fa209eecfad720aeb73958d83d1018e4d19d
Renan <renansantos.silva04@outlook.com>
2024-04-12 10:33:41 -0300

    Challenge #1

--- js/udemy_course01/02_js_fundamentals_part1/script.js
+++ js/udemy_course01/02_js_fundamentals_part1/script.js
@@ -109,4 +109,24 @@ console.log(ageSarah >= 18);
 const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2022);
+
+CHALLENGE #1
+
+// TEST DATA 1
+let massMark = 78;
+let heightMark = 1.69;
+let massJohn = 92;
+let heightJohn = 1.88;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BM1John = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BM1John);
+
+// TEST DATA 2
+massMark = 95;
+heightMark = 1.88;
+massJohn = 85;
+heightJohn = 1.76;
+
+console.log(BMIMark, BM1John);
 */
