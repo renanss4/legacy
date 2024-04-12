bd35f15387e4895c5d913ac88c76b89b3ad2c0c4
Renan <renansantos.silva04@outlook.com>
2024-04-12 08:42:50 -0300

    Values and Variables

--- js/udemy_course01/02_js_fundamentals_part1/script.js
+++ js/udemy_course01/02_js_fundamentals_part1/script.js
@@ -3,3 +3,11 @@ let js = 'amazing';
 if (js === 'amazing') alert('JavaScript is FUN!');
 
 console.log(40+8+23-10);
+
+let country = 'Portugal';
+let continent = 'Europe';
+let population = 10;
+
+console.log(country);
+console.log(continent);
+console.log(population);
