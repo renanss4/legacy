793d6119885911e886bbf45513085baed20c57c4
Renan <renansantos.silva04@outlook.com>
2024-04-12 09:39:21 -0300

    Data Types

--- js/udemy_course01/02_js_fundamentals_part1/script.js
+++ js/udemy_course01/02_js_fundamentals_part1/script.js
@@ -1,8 +1,13 @@
+/*
+
+// Introduction
 
 let js = 'amazing';
 if (js === 'amazing') alert('JavaScript is FUN!');
 
 console.log(40+8+23-10);
+
+// Values and Variables
 
 let country = 'Portugal';
 let continent = 'Europe';
@@ -11,3 +16,34 @@ let population = 10;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// Data Types
+
+// Number: Floating point number -> Used for decimals and integers
+let age = 23;
+
+// String: Sequence of characters -> Used for text
+let firstName = 'Jonas'
+
+// Boolean: Logical type that can only be true or false -> Used for taking decisions
+let fullAge = age;
+
+// Undefined: Value taken by a variable that is not yet defined('empty value')
+let children;
+
+// Null: Also means 'empty value'
+
+// Symbol (ES2015): Value that is unique and cannot be changed [Not useful for now]
+
+// BigInt (ES2020): Larger integers than the Number type can hold
+
+ * JavaScript has dynamic typing: We do not have to manyally define the data type of
+ * the value stored in a variable. Instead, data types are determined automatically.
+
+*/
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+let year;
+console.log(typeof year);
+console.log(typeof null);
