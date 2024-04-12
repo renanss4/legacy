1f5f3dc94a2a3de68c1fe7de6d2ed7a5e82e3d9a
Renan <renansantos.silva04@outlook.com>
2024-04-12 11:31:33 -0300

    Enter

--- js/udemy_course01/02_js_fundamentals_part1/script.js
+++ js/udemy_course01/02_js_fundamentals_part1/script.js
@@ -243,5 +243,17 @@ if (shouldDrive) {
 } else {
     console.log('Someone else should drive...');
 }
-*/
 
+
+const scoreDolphins = (96+108+89)/3;
+const scoreKoalas = (88+91+110)/3;
+
+if (scoreDolphins > scoreKoalas) {
+    console.log(`Dolphins win the trophy`)
+} else if (scoreDolphins < scoreKoalas) {
+    console.log(`Koalas win the trophy`)
+} else {
+    console.log(`Both win the trophy`)
+}
+
+*/
