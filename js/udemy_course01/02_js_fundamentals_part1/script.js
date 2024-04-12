5521117efdf306b00a0290b11c518d49a90fc0f3
Renan <renansantos.silva04@outlook.com>
2024-04-12 11:50:12 -0300

    The Conditional (Ternary) Operator

--- js/udemy_course01/02_js_fundamentals_part1/script.js
+++ js/udemy_course01/02_js_fundamentals_part1/script.js
@@ -256,4 +256,35 @@ if (scoreDolphins > scoreKoalas) {
     console.log(`Both win the trophy`)
 }
 
+
+
+const day = 'monday';
+
+switch (day) {
+    case 'monday': // day === 'monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        // break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednsday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend :D');
+    default:
+        console.log('Not a valid day!');
+}
+
 */
+
+const age = 23;
+// age >= 18 ? console.log('I like to drink wine🍷') : console.log('I like to drink water💧')
+const drink = age >= 18 ? 'wine🍷' : 'water💧';
+console.log(drink)
