d4558c612a12383de51eccf198b0ecc90ab4a774
Renan <renansantos.silva04@outlook.com>
2024-04-23 17:03:30 -0300

    pratica

--- js/udemy_courses/complete_javascript/09_data_structures_modern_operators_and_strings/script.js
+++ js/udemy_courses/complete_javascript/09_data_structures_modern_operators_and_strings/script.js
@@ -48,6 +48,42 @@ const restaurant = {
   },
 };
 
+// 1) Destruturing
+
+// SPREAD, bevause on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+
+//REST, because on LEFT side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+// Objects
+const { sat, ...weekday } = restaurant.openingHours;
+console.log(weekday);
+
+// 2) Functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1);
+
+const x = [23, 5, 7];
+add(...x);
+
+// Use ANY data type, return ANY data type, short-circuiting
+console.log(3 || 'Jonas');
+
 ///////////////////////////////////////
 // String Methods Practice
 
